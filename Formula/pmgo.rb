6de5d1427883b5752c860fa6c7e9205733a4d36b
s0gg <mail@s0gg.net>
2025-01-03 17:50:22 +0900

    chore(brew): Add a file for homebrew

---
+++ Formula/pmgo.rb
@@ -0,0 +1,15 @@
+class Pmgo < Formula
+  desc "HTTP/HTTPS Availability Checker tool written in Go"
+  homepage "https://github.com/s0gg/pmgo"
+  head "https://github.com/s0gg/pmgo.git", branch: "main"
+
+  depends_on "go" => :buildinfo
+
+  def install
+    system "go", "build", *std_go_args, "pmgo.go"
+  end
+
+  test do
+    sytem "#{bin}/pmgo"
+  end
+end
